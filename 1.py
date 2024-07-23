292cf6695d12817dca191b360ad3c0d579786dac
keerthika0909 <167891756+keerthika0909@users.noreply.github.com>
2024-07-23 20:10:34 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+pAF=0.03
+print("the probability that is friday and the student is absent",pAF)
+pF=0.2
+print("the probability that is friday",pF)
+result=(pAF/pF)
+print("the probability that a student is absent given that today is friday",result*100)
